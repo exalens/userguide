1867cd8fb28c3d7c9fc52e4f632d686dabc68b97
sadae <sadaiyandi.ramadoss@exalens.com>
2024-07-16 19:19:03 +0200

    Update Docusaurus config for GitHub Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/favicon.ico',
   colorMode: {
     defaultMode: 'light',
     disableSwitch: true, // Disable the switch to prevent changing to dark mode
