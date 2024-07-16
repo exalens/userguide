4042a2e601fec811a26c6768382f315cb76d950d
sadae <sadaiyandi.ramadoss@exalens.com>
2024-07-17 00:59:40 +0200

    Update Docusaurus config for GitHub Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
 
   // Set the production url of your site here
   // url: 'https://exalens.github.io',
-  url: 'https://docs.exalens.com',
+  url: 'https://exalens.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/userguide',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
