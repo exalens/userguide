d40ca9f561b326b54a13eb09ac3a8bf0cb77adf4
rheartfield <141634113+rheartfield@users.noreply.github.com>
2024-07-17 22:46:35 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  // url: 'https://exalens.github.io',
-  url: 'https://docs.exalens.com',
+  url: 'https://exalens.github.io',
+  // url: 'https://docs.exalens.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/userguide',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
